1b8d7e991721c2976144f479be549fc72a50226c
Team Krypton <casperson.ryan@gmail.com>
2022-11-30 11:16:05 -0500

    Update testst to describe behavior
    I think writing the test case names with exact values doesn't
    tell us much. This change makes it clear which behavior we are
    validating, and the exact values remain test implementation
    details.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,23 @@
 import { getVendingResult } from './index';
 
 describe('getVendingResult', () => {
-  it('should return product B and 14 cents for 1.40 USD with product B selected', () => {
-    expect(getVendingResult('50 50 20 20 B')).toEqual({
-      change: '10 2 2',
-      product: 'B',
-    });
-  });
+  const validCases = [
+    // Typical Case
+    ["Requires Change - A", "50 50 20 20 A", "A", "20 20 5"],
+    ["Requires Change - B", "50 50 20 20 B", "B", "10 2 2"],
+    ["Requires Change - C", "50 50 50 50 50 20 C", "C", "20 10 5 2"],
+    // TODO - more test cases
+  ]
 
-  it('should return product C and no change for 2.33 USD with product C selected', () => {
+  it.each(validCases)("should return the product bought and the correct change with the least amount of coins for a valid command: %p", (testCaseDescription: string, command: string, expectedProduct: string, expectedChange: string) => {
+    expect(getVendingResult(command)).toEqual({
+      change: expectedChange,
+      product: expectedProduct,
+    });
+  })
+
+  it('should return no change if exact change was provided', () => {
+    // TODO - do this for each possible product
     expect(getVendingResult('50 50 50 50 20 10 2 1 C')).toEqual({
       change: null,
       product: 'C',
@@ -22,13 +31,16 @@ describe('getVendingResult', () => {
     });
   });
 
-  // Command validation
+  // Command Validation
   const invalidCommands = [
     ["Invalid product: 'command'", "Invalid command"],
     // Valid coins, but invalid product
     ["Invalid product: 'Q'", "1 2 50 Q"],
+    ["Invalid product: '1'", "1 2 50 1"],
     // Valid product, but invalid coins
-    ["Coin value is not a number: 'A'", "A 2 50 A"],
+    ["Coin value is not an integer: 'A'", "A 2 50 A"],
+    ["Coin value is not an integer: '.5'", ".5 2 50 A"],
+    ["Coin value is not a valid number: '11'", "11 2 50 A"],
   ]
   it.each(invalidCommands)("should throw an error containing '%p' with the command '%p'", (expectedError: string, command: string) => {
     expect(() => {
